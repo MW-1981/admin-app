71d408d025f6f3afba9fbc5bc5268ec22c3a6aed
Mitsutaka <esthersoftwarelabo@gmail.com>
2021-09-21 15:44:44 +1200

    Get members data by http client

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -3,6 +3,7 @@ import { Observable, of } from 'rxjs';
 import { Member } from './member';
 import { MEMBERS } from './mock-members';
 import { MessageService } from './message.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   // root: can access this service from all this app.
@@ -10,7 +11,12 @@ import { MessageService } from './message.service';
 })
 export class MemberService {
 
-  constructor(private messageService: MessageService) { }
+  private membersUrl = 'api/members';
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
 
   // getMembers(): Member[]{
   //   return MEMBERS
@@ -19,11 +25,16 @@ export class MemberService {
   // use async method by 'rxjs'
   getMembers(): Observable<Member[]> {
     this.messageService.add("MemberService: Member list is loaded.");
-    return of(MEMBERS);
+    return this.http.get<Member[]>(this.membersUrl);
+//    return of(MEMBERS); // rxjs
   }
 
   getMember(id: number): Observable<Member> {
     this.messageService.add(`MemberService: Get Member data No. ${id}.`);
     return of(MEMBERS.find(member => member.id === id));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`MemberService: ${message}`);
   }
 }
